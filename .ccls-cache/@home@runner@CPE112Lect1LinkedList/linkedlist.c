a96a72afbce18f381cdf5efbc02fabfef77a1ee1
WTFPUn <48788884+WTFPUn@users.noreply.github.com>
2022-01-25 09:49:13 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@CPE112Lect1LinkedList/linkedlist.c
@@ -0,0 +1,146 @@
+#include "linkedlist.h"
+#include <stdlib.h>
+
+/* Create a new list with the specify capacity
+ * Return pointer to the list structure if successful
+ *        or NULL if the list can't be created 
+ *        e.g. memory allocation error  
+ */
+LIST_T* listCreate(int capacity)
+{
+    LIST_T* list = (LIST_T *) calloc(1, sizeof(LIST_T));
+    return list;
+}
+
+/* Destroy the list and release all memory
+ */
+void listDestroy(LIST_T* list)
+{
+    free(list->current);
+    free(list->head);
+    free(list->tail);
+}
+
+/* Returns the number of elements (could be zero)
+ * or -1 if the list has not been created
+ */
+int listSize(LIST_T* list)
+{
+  return list->
+}
+
+/* Insert new element into the list at the specify index
+ * Return -1 if the list hasn't been created
+ *        -2 if the list is full 
+ *         1 if the operation is successful
+ */
+int listInsertAt(LIST_T* list, int index, void* data)
+{
+
+}
+
+/* Add new element at the end of the list
+ * Return -1 if the list hasn't been created
+ *        -2 if the memory allocation error 
+ *         1 if the operation is successful
+ */
+int listAppend(LIST_T* list, void* data)
+{
+    if (list == NULL)
+    {
+        return -1;
+    }
+
+    LISTNODE_T* node = (LISTNODE_T*) calloc(1, sizeof(LISTNODE_T));
+    if (node == NULL)
+    {
+        return -2;
+    }
+    node->data = data;
+
+    if (list->head == NULL && list->tail == NULL)
+    {
+        list->head = node;
+        list->tail = node;
+    }
+    else
+    {
+        list->tail->next = node;
+        list->tail = node;
+    }
+    return 1;
+}
+
+/* Remove an element at the specify index
+ * Return -1 if the list hasn't been created
+ *         1 if the operation is successful
+ */
+int listRemoveAt(LIST_T* list, int index)
+{
+    LISTNODE_T* previousNode = list->head;
+    LISTNODE_T* nodeToBeRemove;
+
+    if (index == 0)
+    {
+        nodeToBeRemove = list->head;
+        list->head = nodeToBeRemove->next;
+        free(nodeToBeRemove->data);
+        free(nodeToBeRemove);
+    }
+    else
+    {
+        int i = 0;
+        while (i < index-1)
+        {
+            previousNode = previousNode->next;
+            i++;
+        }
+
+        nodeToBeRemove = previousNode->next;
+        previousNode->next = nodeToBeRemove->next; 
+        if (nodeToBeRemove == list->tail) 
+        {
+            list->tail = previousNode;
+        }
+        free(nodeToBeRemove->data);
+        free(nodeToBeRemove);
+    }
+}
+
+/* Get an element at the specify index
+ * Return -1 if the list hasn't been created
+ *         1 if the operation is successful
+ */
+int listGet(LIST_T* list, int index, void** data)
+{
+
+}
+
+/* Reset the current element return by listGetNext to 
+ * the first element of the list
+ * Return -1 if the list hasn't been created
+ *         1 if the operation is successful
+ */
+int listReset(LIST_T* list)
+{
+
+}
+
+/* Get the current element
+ * Return -1 if the list hasn't been created
+ *         1 if the operation is successful
+ */
+int listGetNext(LIST_T* list, void** data)
+{
+
+}
+
+/* 
+ * Return -1 if the list hasn't been created
+ *         0 if there still some elements left to be returned from listGetNext
+ *         1 if there isn't any element to be returned from listGetNext
+ */
+int listAtEnd(LIST_T* list)
+{
+
+}
---
+++ linkedlist.h
@@ -0,0 +1,79 @@
+#ifndef ARRAYLIST_H
+#define ARRAYLIST_H
+
+typedef struct _listnode
+{
+    void* data;                 /* data of this node */
+    struct _listnode* next;     /* link to the next node */ 
+} LISTNODE_T;
+
+typedef struct
+{
+    LISTNODE_T* head;           /* first item in list */  
+    LISTNODE_T* tail;           /* last item in list */ 
+    LISTNODE_T* current;        /* current item in list */  
+} LIST_T;
+
+/* Create a new list with the specify capacity
+ * Return pointer to the list structure if successful
+ *        or NULL if the list can't be created 
+ *        e.g. memory allocation error  
+ */
+LIST_T* listCreate(int capacity);
+
+/* Destroy the list and release all memory
+ */
+void listDestroy(LIST_T* list);
+
+/* Returns the number of elements (could be zero)
+ * or -1 if the list has not been created
+ */
+int listSize(LIST_T* list);
+
+/* Insert new element into the list at the specify index
+ * Return -1 if the list hasn't been created
+ *        -2 if the list is full 
+ *         1 if the operation is successful
+ */
+int listInsertAt(LIST_T* list, int index, void* data);
+
+/* Add new element at the end of the list
+ * Return -1 if the list hasn't been created
+ *        -2 if the list is full 
+ *         1 if the operation is successful
+ */
+int listAppend(LIST_T* list, void* data);
+
+/* Remove an element at the specify index
+ * Return -1 if the list hasn't been created
+ *         1 if the operation is successful
+ */
+int listRemoveAt(LIST_T* list, int index);
+
+/* Get an element at the specify index
+ * Return -1 if the list hasn't been created
+ *         1 if the operation is successful
+ */
+int listGet(LIST_T* list, int index, void** data);
+
+/* Reset the current element return by listGetNext to 
+ * the first element of the list
+ * Return -1 if the list hasn't been created
+ *         1 if the operation is successful
+ */
+int listReset(LIST_T* list);
+
+/* Get the current element
+ * Return -1 if the list hasn't been created
+ *         1 if the operation is successful
+ */
+int listGetNext(LIST_T* list, void** data);
+
+/* 
+ * Return -1 if the list hasn't been created
+ *         0 if there still some elements left to be returned from listGetNext
+ *         1 if there isn't any element to be returned from listGetNext
+ */
+int listAtEnd(LIST_T* list);
+
+#endif
